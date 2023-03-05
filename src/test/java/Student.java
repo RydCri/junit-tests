6ead229731463f121174ccc0c90e56f9726ad9ce
RydCri <107228115+RydCri@users.noreply.github.com>
2023-03-05 17:40:57 -0600

    fields added

---
+++ src/test/java/Student.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Student {
+    public long id;
+    public String name;
+    public ArrayList<Integer> grades;
+    private Student(long id,String name, ArrayList<Integer> grades ){
+    }
+}
