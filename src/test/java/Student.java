14680ec5faeb92e3ad4ca667a845ebbd78f8cd85
RydCri <107228115+RydCri@users.noreply.github.com>
2023-03-05 17:55:07 -0600

    getInfo prints Student instance fields

--- src/test/java/Student.java
+++ src/test/java/Student.java
@@ -1,9 +1,29 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Student {
     public long id;
     public String name;
     public ArrayList<Integer> grades;
     private Student(long id,String name, ArrayList<Integer> grades ){
+        this.id = id;
+        this.name = name;
+        this.grades = grades;
+    }
+    public void addGrade(int grade){
+        this.grades.add(grade);
+    }
+    public void getInfo(){
+        System.out.printf("Student Id: %s\nName: %s\nGrades: %s",this.id,this.name,this.grades);
+    }
+
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student(1,"Jim",new ArrayList<>());
+        s1.addGrade(33);
+        s1.addGrade(44);
+        s1.addGrade(77);
+      s1.getInfo();
     }
 }
