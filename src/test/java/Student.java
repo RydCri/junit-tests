670ff3aba8d50efdcc2cc60d33f09cf629ee1742
RydCri <107228115+RydCri@users.noreply.github.com>
2023-03-05 18:12:03 -0600

    getGradeAverage returns double formatted to 2 decimals

--- src/test/java/Student.java
+++ src/test/java/Student.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,11 +11,42 @@ public class Student {
         this.name = name;
         this.grades = grades;
     }
+
+    // returns the student's id
+    public long getId(){
+        return this.id;
+    }
+
+    // returns the student's name
+    public String getName(){
+        return this.name;
+    }
+
+    // adds the given grade to the grades list
     public void addGrade(int grade){
         this.grades.add(grade);
     }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades(){
+        return this.grades;
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+            double sum = 0;
+            double total;
+            List<Integer> x = this.grades;
+            for(int i = 0; i < x.size(); i++){
+                sum += x.get(i);
+            }
+            total = sum / x.size();
+            DecimalFormat dF = new DecimalFormat("0.##");
+                return Double.parseDouble(dF.format(total));
+        }
     public void getInfo(){
-        System.out.printf("Student Id: %s\nName: %s\nGrades: %s",this.id,this.name,this.grades);
+        System.out.printf("Student Id: %s\nName: %s\nGrades: %s\n",this.id,this.name,this.grades);
+        System.out.println("Grade Average: " + this.getGradeAverage());
     }
 
 
